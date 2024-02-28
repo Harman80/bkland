41539923d1a197874d16680f2c98312920741b96
Harjit Singh <harjit80@gmail.com>
2024-02-28 14:52:05 +0530

    BK Land Developers Final Website

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"64#|A>ezg[FwhTbpABx P&rJ3ae5/0<FJZLZl 5U9dxgfg0iFGV8tH[4DgItC<XS";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:1798;}
